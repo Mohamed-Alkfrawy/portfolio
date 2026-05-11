958bd365d1000930c69a2bfb5693bde3895f50fc
Ahmed Salama <as.theideal@gmail.com>
2026-05-11 07:33:43 +0300

    minor update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import {NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Nav (){
-    return(
-        <nav className='flex'>
-            <ul className='flex'>
-                <li><NavLink to="portfolio/">Home</NavLink></li>
-                <li><NavLink to="portfolio/about">About</NavLink></li>
-                <li><NavLink to="portfolio/contact">Contact</NavLink></li>
-            </ul>
-        </nav>
-    )
+function Nav() {
+  return (
+    <nav className="flex">
+      <ul className="flex">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-export default Nav
+export default Nav;
